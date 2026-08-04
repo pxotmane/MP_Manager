b2692cff6767f218218beb7216c53195b22491f8
Otmane <pxotmane@gmail.com>
2026-08-04 07:42:51 +0100

    third commit

--- marche/views.py
+++ marche/views.py
@@ -3,11 +3,11 @@ from .models import FicheMarche, Nantissement, Penalite
 # Create your views here.
 def marche_table(request):
     # # Fetch all FicheMarche objects from the database
-    # marches = FicheMarche.objects.all()
+    marches = FicheMarche.objects.all()
     
     # # Pass the fetched data to the template
     # context = {
     #     'marches': marches,
     # }
     
-    return render(request, 'marche/marche_table.html')
+    return render(request, 'marche/tableau_marche.html', {'marches': marches})
