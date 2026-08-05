94153eebd61d1a14a3d0e4ff7ecf79bf391d5223
Otmane <pxotmane@gmail.com>
2026-08-06 00:16:17 +0100

    login template commit

--- MP_Manager/urls.py
+++ MP_Manager/urls.py
@@ -21,7 +21,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     # Authentification
-    path('login/', auth_views.LoginView.as_view(template_name='login/login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', include('pages.urls')), # Include the URLs from the 'pages' app
     path('tresorerie/', include('tresorerie.urls')), # Include the URLs from the 'tresorerie' app
--- login/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-# urlpatterns = [
-#     path("login/", views.LoginUser.as_view(), name="login"),
-#     path("logout/", views.LogoutUser.as_view(), name="logout"),
-# ]
